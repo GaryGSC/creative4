79f09cd63b645ddf1678b3764c7373174c22c847
GaryGSC <garygsc@gmail.com>
2017-03-14 18:16:40 -0600

    Added example arrays that we'll pass around

---
+++ examples.js
@@ -0,0 +1,24 @@
+var sample_post = {
+    guess: ["RED", "GREEN", "RED", "GREEN"]
+};
+
+var sample_response = {
+    guesses: [
+        {
+            guess: ["RED", "BLUE", "RED", "GREEN"],
+            response: "SSC "
+        },
+        {
+            guess: ["RED", "BLUE", "RED", "GREEN"],
+            response: "SSS "
+        },
+        {
+            guess: ["RED", "BLUE", "RED", "GREEN"],
+            response: "SSS "
+        }
+    ]
+};
+
+var sample_set_code_post = {
+    code: ["RED", "GREEN", "RED", "GREEN"]
+};
