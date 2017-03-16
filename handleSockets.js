76d4ac8431cf9ccb2339a99d791ba7ca3044de2d
GaryGSC <garygsc@gmail.com>
2017-03-16 13:02:16 -0600

    Did some work on backend.

--- handleSockets.js
+++ handleSockets.js
@@ -15,10 +15,11 @@ module.exports = function(client){
 		name = player_name ? player_name : client.id;
 		rooms[room_id] = {
 			host_name: name,
-			host: client
+			host: client,
+			guesses: []
 		};
 		in_game = room_id;
-		console.log("New game created. The room ID is:", room_id);
+		console.log("New game created by "+name+". The room ID is:", room_id);
 		client.emit("game_created", room_id)
 	});
 
@@ -28,7 +29,7 @@ module.exports = function(client){
 			name = params.name ? params.name : client.id;
 			rooms[params.room_id].player_name = name;
 			rooms[params.room_id].player = client;
-			in_game = true;
+			in_game = params.room_id;
 			console.log(name + " joined room " + params.room_id + ".");
 			client.join(params.room_id);	//Join the room (will be used for chat between players and potentially spectators)
 			client.to(params.room_id).emit("player_joined", name); //Essentially, tell the chatroom that another player joined.
@@ -48,10 +49,79 @@ module.exports = function(client){
 		}
 	});
 
+	client.on("set_code", function(code){
+		console.log("The code is:", code);
+		var room_id = in_game;
+		rooms[room_id].code = code;
+		if (rooms[room_id].host_is_mastermind){
+			rooms[room_id].host.emit("code_set", rooms[room_id].host_name);
+			rooms[room_id].player.emit("code_was_set", rooms[room_id].host_name);
+		} else {
+			rooms[room_id].player.emit("code_set", rooms[room_id].player_name);
+			rooms[room_id].host.emit("code_was_set", rooms[room_id].player_name);
+		}
+	});
+
+	client.on("make_guess", function(guess){
+		console.log(name + " guessed " + JSON.stringify(guess));
+
+		var this_guess = {
+			guess: []
+		};
+		this_guess.guess[0] = guess[0];
+		this_guess.guess[1] = guess[1];
+		this_guess.guess[2] = guess[2];
+		this_guess.guess[3] = guess[3];
+
+		var temp_code = [];
+		temp_code[0] = rooms[in_game].code[0];
+		temp_code[1] = rooms[in_game].code[1];
+		temp_code[2] = rooms[in_game].code[2];
+		temp_code[3] = rooms[in_game].code[3];
+
+		var response = "";
+		for (var i = 0; i < 4; i++){
+			if (guess[i] == temp_code[i]){
+				response += "S";
+				temp_code[i] = "TAKEN";
+				guess[i] = "TAKEN";
+			}
+		}
+		for (var i = 0; i < 4; i++){
+			if (guess[i] != "TAKEN"){
+				for (var j = 0; j < 4; j++){
+					if (guess[i] == temp_code[j]){
+						response += "C";
+						temp_code[j] = "TAKEN";
+						break;
+					}
+				}
+			}
+		}
+		response = (response + "    ").substring(0, 4);
+		console.log("Response:", response);
+
+		this_guess.response = response;
+		rooms[in_game].guesses.push(this_guess);
+		var board_state = {
+			guesses: rooms[in_game].guesses
+		};
+		console.log("State of game:", board_state);
+		rooms[in_game].host.emit("board_updated", board_state);
+		rooms[in_game].player.emit("board_updated", board_state);
+
+		if (response == "SSSS"){
+			rooms[in_game].host.emit("code_was_guessed");
+			rooms[in_game].player.emit("code_was_guessed");
+		}
+	});
+
 	client.on('disconnect', function(){
 		if (in_game) { //in_game is the room_id if playing (not just a spectator) or "" (which evaluates to false)
-			client.to(in_game).emit("a_player_left", name);
+			rooms[in_game].host.emit("a_player_left", name);
+			rooms[in_game].player.emit("a_player_left", name);
 		}
+		console.log("Client " + client.id + " disconnected.");
 	});
 };
 
@@ -62,4 +132,13 @@ function getRandomRoomID(){
 function startGame(room_id){
 	rooms[room_id].game = 1;
 	rooms[room_id].host_is_mastermind = !!(Math.round(Math.random())); //Evaluates to true or false, randomly. (!! is a hacky way to convert to boolean)
+	if (rooms[room_id].host_is_mastermind){
+		console.log(rooms[room_id].host_name + " is the mastermind!");
+		rooms[room_id].host.emit("you_should_set_code", rooms[room_id].host_name);
+		rooms[room_id].player.emit("other_player_is_setting_code", rooms[room_id].host_name);
+	} else {
+		console.log(rooms[room_id].player_name + " is the mastermind!");
+		rooms[room_id].player.emit("you_should_set_code", rooms[room_id].player_name);
+		rooms[room_id].host.emit("other_player_is_setting_code", rooms[room_id].player_name);
+	}
 }
--- public/javascripts/mmApp.js
+++ public/javascripts/mmApp.js
@@ -1,6 +1,4 @@
-angular.module('mmApp', []);
-
-angular.module('mmApp').controller('mmCtrl', function($scope) {
+angular.module('mmApp', []).controller('mmCtrl', function($scope) {
 
     var socket = io();
 
@@ -28,7 +26,7 @@ angular.module('mmApp').controller('mmCtrl', function($scope) {
                     {id: 1, value: "RED"},
                     {id: 2, value: "YELLOW"},
                     {id: 3, value: "RED"},
-                    {id: 4, value:"GREEN"}
+                    {id: 4, value: "GREEN"}
                  ],
                 response: "SSC "
             },
@@ -37,7 +35,7 @@ angular.module('mmApp').controller('mmCtrl', function($scope) {
                     {id: 1, value: "RED"},
                     {id: 2, value: "YELLOW"},
                     {id: 3, value: "RED"},
-                    {id: 4, value:"GREEN"}
+                    {id: 4, value: "GREEN"}
                  ],
                 response: "SSS "
             },
@@ -46,7 +44,7 @@ angular.module('mmApp').controller('mmCtrl', function($scope) {
                     {id: 1, value: "RED"},
                     {id: 2, value: "YELLOW"},
                     {id: 3, value: "RED"},
-                    {id: 4, value:"PURPLE"}
+                    {id: 4, value: "PURPLE"}
                  ],
                 response: "SSS "
             }
@@ -55,45 +53,96 @@ angular.module('mmApp').controller('mmCtrl', function($scope) {
 
 
     $scope.submitColorCode = function() {
-        console.log($scope.colorCode);
-    }
+        socket.emit('set_code', $scope.colorCode.code);
+        console.log("Set code to: " + $scope.colorCode.code);
+    };
 
 	$scope.submitGuess = function(){
-		console.log($scope.guess);
+	    socket.emit('make_guess', $scope.guess.guess);
+		console.log("Guess: " + $scope.guess.guess);
         //Need to submit scope.guess to Server
         //And handle response
         //response data will replace scope.sampleresponse
 
         // Handle winning response......
-	}
+	};
 
 
 
-    var createGame = function() {
-        var name = $scope.userName; //Somehow want to give the player a way to set their name.
-        socket.emit('create_game', name);
-        socket.on('game_created', function(room_id){
-            //Will probably want to give them some kind of message that says something like this
-            //Maybe put some text in a div or span?
-            $scope.message="Game successfully created. Your room ID is: " + room_id + "<br />Please select the color code for your opponent.";
-            $scope.showColorCodeBox = true;
-        });
-    }
+    $scope.createGame = function() {
+        socket.emit('create_game', $scope.userName);
+    };
 
-    var joinGame = function() {
+    $scope.joinGame = function() {
         var parameters = {
             room_id: $scope.roomId,
             name: $scope.userName
         };
         socket.emit('join_game', parameters);
-        socket.on('successfully_joined', function(){
-            $scope.message="Game successfully joined.";
-        });
-        socket.on("failed_to_join", function(){
-            $scope.message="Unable to join game.";
-        });
-    }
+    };
 
+	socket.on('game_created', function(room_id){
+	    console.log("Game successfully created. Your join code is: "+room_id);
+        $scope.roomId = room_id;
+		$scope.message="Game successfully created. Your join code is: " + room_id;
+		$scope.showColorCodeBox = true;
 
+		//DISABLE CREATE/JOIN GAME BUTTONS
+	});
 
+	socket.on('successfully_joined', function(){
+	    console.log("Successfully joined game "+$scope.roomId);
+		$scope.message="Game successfully joined.";
+
+		//DISABLE CREATE/JOIN GAME BUTTONS
+	});
+
+	socket.on("failed_to_join", function(){
+	    console.log("Failed to join game "+$scope.roomId);
+		$scope.message="Unable to join game.";
+	});
+
+	socket.on("you_should_set_code", function(){
+	    console.log("You're the mastermind!");
+		$scope.message="You've been selected as the mastermind! Please submit the color code!";
+
+		//ENABLE SUBMIT COLOR CODE BUTTON
+	});
+
+	socket.on("other_player_is_setting_code", function(){
+	    console.log("The other player is the mastermind!");
+		$scope.message="The other player was selected as the mastermind! Please wait while they submit the color code!";
+	});
+
+	socket.on("code_set", function(){
+	    console.log("Successfully set the color code. Now it's time for the other player to guess it.");
+	    $scope.message="Successfully set the color code. Now it's time for the other player to guess it.";
+
+	    //DISABLE SUBMIT COLOR CODE BUTTON
+    });
+
+    socket.on("code_was_set", function(){
+        console.log("The other player set the code. Now it's time for you to break it!");
+        $scope.message="The other player set the code. Now it's time for you to break it!";
+
+        //ENABLE SUBMIT GUESS BUTTON
+    });
+
+    socket.on("board_updated", function(board_state){
+        console.log("Board now looks like this:", JSON.stringify(board_state));
+
+        //UPDATE APPEARANCE OF BOARD BASED ON board_state
+    });
+
+    socket.on("code_was_guessed", function(){
+        console.log("The code was guessed!");
+
+        //GAME OVER? ENABLE/DISABLE STUFF?
+    });
+
+    socket.on("a_player_left", function(){
+        console.log("The other player left.");
+
+        //GAME OVER
+    })
 });
